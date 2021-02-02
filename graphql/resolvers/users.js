ae2b4e49f39acc77c0a20c526acd72a2549cabaf
seoa <andrew.d.seo@gmail.com>
2021-02-02 16:30:10 -0700

    added user data validation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
 
+const { validateRegisterInput } = require('../../util/validators');
 const { SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
 
@@ -14,6 +15,10 @@ module.exports = {
             }
         ) {
             // TODO: Validate user data
+            const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword);
+            if (!valid) {
+                throw new UserInputError('Errors', { errors });
+            }
             // TODO: Make sure user doesnt already exist
             const user = await User.findOne({ username });
             if (user) {
